36972af4137b3aababbd7d5e2fef8152bb84a32e
Ruslan Kosenkov <r.kosenkov@spb.mcart.ru>
2022-06-03 13:56:12 +0300

    Added components: Modal and ModalValidate. Installed vuelidate

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="wrapper">
+    <!-- Header -->
+
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <!-- First Modal -->
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
+            Show first modal
+          </button>
+          <modal
+            title="First Modal"
+            v-show="modalFirst"
+            @close="modalFirst = !modalFirst"
+          >
+            <div slot="body">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
+                fuga excepturi reiciendis tenetur aperiam repudiandae?
+              </p>
+              <button class="btn btnPrimary" @click="modalFirst = false">
+                Well done!
+              </button>
+            </div>
+          </modal>
+
+          <!-- Second modal -->
+          <button
+            class="btn btnPrimary"
+            @click="modalSecond.show = !modalSecond.show"
+          >
+            Show modal with form
+          </button>
+          <modal
+            title="Modal with form"
+            v-show="modalSecond.show"
+            @close="modalSecond.show = false"
+          >
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input type="text" required v-model="modalSecond.name" />
+                <label>Email:</label>
+                <input type="email" required v-model="modalSecond.email" />
+                <button class="btn btnPrimary">Submit</button>
+              </form>
+            </div>
+          </modal>
+
+          <!-- Modal with validate -->
+          <button
+            class="btn btnPrimary"
+            @click="modalValidate = !modalValidate"
+          >
+            Show modal with form + validate
+          </button>
+          <modalValidate
+            v-show="modalValidate"
+            @close="modalValidate = false"
+          >
+          </modalValidate>
+        </div>
+      </section>
+    </div>
+
+    <!-- Footer -->
   </div>
 </template>
 
 <script>
+import modal from '@/components/UI/Modal.vue'
+import modalValidate from '@/components/ModalValidate.vue'
 export default {
   name: 'App',
+  components: {
+    modal,
+    modalValidate,
+  },
+
+  data() {
+    return {
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: '',
+      },
+      modalValidate: false,
+    }
+  },
+
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email,
+      })
+
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
+    },
+  },
 }
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/ModalValidate.vue
@@ -0,0 +1,27 @@
+<template>
+  <modal
+    title="Modal with form + validate"
+    @close="$emit('close')"
+  >
+    <div slot="body">
+      <form @submit.prevent="">
+        <label>Name:</label>
+        <input type="text" required />
+        <label>Email:</label>
+        <input type="email" required />
+        <button class="btn btnPrimary">Submit</button>
+      </form>
+    </div>
+  </modal>
+</template>
+
+<script>
+import modal from '@/components/UI/Modal.vue'
+export default {
+  components: {
+    modal,
+  },
+}
+</script>
+
+<style></style>
